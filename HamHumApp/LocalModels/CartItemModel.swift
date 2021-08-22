92e454993b5d50e0583cb75f82b5b2efa2a7f6d8
Samet ALEMDAROGLU <smtalmdr@icloud.com>
2021-08-22 23:08:50 +0300

    wip

---
+++ HamHumApp/LocalModels/CartItemModel.swift
@@ -0,0 +1,16 @@
+//
+//  CartItem.swift
+//  HamHumApp
+//
+//  Created by ALEMDAR on 22.08.2021.
+//
+
+import Foundation
+
+struct CartItemModel {
+    let name: String?
+    let image: String?
+    let count: Int?
+    let price: Double?
+    let currency: String?
+}
